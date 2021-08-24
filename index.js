110234d1330cc55bc67fc1cf9a7331751b2cf3d1
Vanessa Gellert <nessagellert@gmail.com>
2021-08-24 00:51:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var idade
+
+idade = parseInt(prompt('Informe a sua idade'))
+
+alert(idade)
+if(idade >= 16){
+  alert('Você pode votar!')
+}
+else{
+  alert('Você precisa esperar até completar 16 anos para poder votar!')
+}
